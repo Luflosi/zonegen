a6cb42fcaee1ca3de00fb13a5e262c82912e30ff
Luflosi <luflosi@luflosi.de>
2025-05-21 00:17:40 +0200

    Create zizmor input files differently
    The directory structure using symlinks won't work in newer zizmor versions anymore for some reason.

--- flake.nix
+++ flake.nix
@@ -109,10 +109,10 @@
 
           zonegen-zizmor = pkgs.runCommand "run-zizmor" {
             # zizmor needs this folder structure for some reason
-            src = pkgs.linkFarm "repo" [{
-              name = ".github/workflows";
-              path = ./.github/workflows;
-            }];
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = ./.github/workflows;
+            };
           } ''
             '${lib.getExe pkgs.zizmor}' --offline "$src"
             touch "$out"
